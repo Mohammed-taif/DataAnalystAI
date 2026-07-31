28964f4b75a54f7169b548854694520d7a5b496c
Mohammed Taif <mohammedtaif@Mohammeds-MacBook-Air.local>
2026-07-31 10:21:59 +0530

    Complete React dashboard with AI chat and charts

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from backend.api.upload import router as upload_router
 from backend.api.ask import router as ask_router
 
@@ -7,6 +7,15 @@ app = FastAPI(
     title="DataAnalystAI",
     description="AI Powered Data Analyst",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(upload_router)
